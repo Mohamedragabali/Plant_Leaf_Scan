98351177c3bcf872ef119523dcbed979f07d2427
Mohamed Ragab <mohamed.ragab.ali.9.@gmail.com>
2024-06-02 17:34:18 +0300

    ci:add view binding

--- app/src/main/java/com/mohamedragabali/plantleafscan/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mohamedragabali/plantleafscan/ui/home/HomeFragment.kt
@@ -5,14 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mohamedragabali.plantleafscan.R
+import com.mohamedragabali.plantleafscan.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
