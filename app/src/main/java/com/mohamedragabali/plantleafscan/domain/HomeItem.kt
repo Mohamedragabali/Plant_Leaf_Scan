bc97a48e11e4775faa00dd192a5689284c609c55
Mohamed Ragab <mohamed.ragab.ali.9.@gmail.com>
2024-06-03 23:41:33 +0300

    refactor:refactor code

--- app/src/main/java/com/mohamedragabali/plantleafscan/domain/HomeItem.kt
+++ app/src/main/java/com/mohamedragabali/plantleafscan/domain/HomeItem.kt
@@ -2,7 +2,6 @@ package com.mohamedragabali.plantleafscan.domain
 
 sealed class HomeItem(val type: HomeItemType) {
     class Header : HomeItem(HomeItemType.HEADER)
-    data class Slider(val slideText :List<String>): HomeItem(HomeItemType.SLIDER)
-    data class PlantName(val plantNames: List<String>): HomeItem(HomeItemType.PLANT_NAME)
-    data class Plant(val plant:List<Pair<Int,String>>): HomeItem(HomeItemType.PLANT)
+    data class Slider(val slideText: List<String>) : HomeItem(HomeItemType.SLIDER)
+    data class Plant(val plant: List<Pair<Int, String>>  , val plantNameText : String) : HomeItem(HomeItemType.PLANT)
 }
