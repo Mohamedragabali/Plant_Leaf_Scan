488b61774834ee588699fd4169c82f95369d2efc
Mohamed Ragab <mohamed.ragab.ali.9.@gmail.com>
2024-06-03 23:46:06 +0300

    ci:add the implement of search

--- app/src/main/java/com/mohamedragabali/plantleafscan/ui/search/SearchFragment.kt
+++ app/src/main/java/com/mohamedragabali/plantleafscan/ui/search/SearchFragment.kt
@@ -4,14 +4,41 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.fragment.app.Fragment
 import com.mohamedragabali.plantleafscan.R
+import com.mohamedragabali.plantleafscan.databinding.FragmentHomeBinding
+import com.mohamedragabali.plantleafscan.databinding.FragmentSearchBinding
+import com.mohamedragabali.plantleafscan.domain.data.DataSource
 
-class SearchFragment : Fragment() {
+class SearchFragment : Fragment()  {
+    lateinit var binding: FragmentSearchBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter = SearchAdapter(emptyList())
+        binding.searchRecyclerView.adapter = adapter
+        binding.searchView.setOnQueryTextListener(object :OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+               return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if(newText != null ){
+                   val list =  DataSource.getPlantDiseaseHasText(newText)
+                    adapter.setData(list)
+                }
+                if(newText == ""){adapter.setData(emptyList())}
+               return true
+            }
+        })
+
     }
 }
