5195650d23e62139683518adcd5369655b00b1db
Mohamed Ragab <mohamed.ragab.ali.9.@gmail.com>
2024-06-03 23:32:54 +0300

    build:add dependencies of material3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,5 +56,9 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
 
+    implementation("androidx.compose.material3:material3:1.3.0-beta01")
+    implementation ("com.google.android.material:material:1.12.0")
+
+
 
 }
